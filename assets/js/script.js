d4edead5bbae69f0ffd01f8332332e02434d42de
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2024-09-23 04:50:14 +0000

    Add code to calculate increment score and incorrect tally answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,12 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        //increment score function called here.
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        //increment wrong answer funtion called here.
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,11 +81,22 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1.
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
